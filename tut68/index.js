ebfbb2d1dadac1d66a3809eb8a87094d15bd5b63
chinmay021 <chinmaykumar021@gmail.com>
2020-07-15 19:07:19 +0530

    importing custom modules

---
+++ tut68/index.js
@@ -0,0 +1,6 @@
+// const average = require('./mod');
+// console.log(average([1, 2]));
+const mod = require('./mod');
+console.log(mod.avg([10, 20]));
+console.log(mod);
+console.log('This is index.js');
